7e0aec6d6f3c0238187bb3a104aa72abe679dc83
William Bainton <baintonw@gmail.com>
2019-05-07 19:28:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,7 +27,7 @@ end
 def bulk_tweet_shortener(tweets)
   
   tweets.collect do |twit|
-    
+    binding.pry
   one_tweet = twit.split(" ")
   one_tweet.collect do |word|
     if dictionary.keys.include?(word)
