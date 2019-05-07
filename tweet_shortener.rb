8340103d1995f1667a3bff9fd2f2d5de1c390f0f
William Bainton <baintonw@gmail.com>
2019-05-07 21:23:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,6 +53,6 @@ end
 def shortened_tweet_truncator(tweets)
 
   if word_substituter(tweets).length >= 140
-   puts "#{word_substituter(tweets)[0..140]}..."
+    "#{word_substituter(tweets)[0..140]}..."
   end
 end
