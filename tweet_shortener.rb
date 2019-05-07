740c8f61c88128458654e7014cd38e69f7b26549
William Bainton <baintonw@gmail.com>
2019-05-07 21:49:41 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,60 @@
-# Write your code here.
+require 'pry'
+def dictionary
+  {"hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "For" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  tweet_array.collect do |word|
+    if dictionary.keys.include?(word)
+      dictionary[word]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.collect do |twit|
+   puts "#{word_substituter(twit)}"
+  end
+end
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.collect do |twit|
+   puts "#{word_substituter(twit)}"
+  end
+end
+
+
+def selective_tweet_shortener(tweets)
+  
+    if tweets.length <= 140
+      tweets
+    else
+      word_substituter(tweets)
+    end
+end
+
+def shortened_tweet_truncator(tweets)
+
+  if word_substituter(tweets).length >= 140
+    "#{word_substituter(tweets)[0..136]...}"
+  else
+    "#{word_substituter(tweets)}"
+  end
+end
